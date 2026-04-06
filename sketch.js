73072bbaff2738deef34df14ce02b0db62c3febb
sachin9619 <79572006+sachin9619@users.noreply.github.com>
2026-04-07 00:20:45 +0530

    Comment out firebase database initialization
    Comment out database initialization in setup function

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function preload(){
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
-  database = firebase.database();
+  //database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
