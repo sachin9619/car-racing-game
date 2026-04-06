b34aa5d63610c869ab2fd9d75f9d04c1ddca7580
sachin9619 <79572006+sachin9619@users.noreply.github.com>
2026-04-07 00:38:40 +0530

    Refactor reset button and adjust element positions
    Updated the reset button functionality to reset player count and game state, and adjusted element positions.

--- Form.js
+++ Form.js
@@ -5,8 +5,9 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
-    this.reset=createButton("RESET")
+    this.reset = createButton("RESET");
   }
+
   hide(){
     this.greeting.hide();
     this.button.hide();
@@ -16,26 +17,30 @@ class Form {
 
   display(){
     this.title.html("Car Racing Game");
-    this.title.position(displayWidth/2 - 50, 0);
+    this.title.position(displayWidth/2 - 100, 20);
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-    this.reset.position(displayWidth/2,20)
+    this.reset.position(displayWidth/2, 60);
 
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
+
+      // LOCAL (no database)
       player.name = this.input.value();
-      playerCount+=1;
+      playerCount += 1;
       player.index = playerCount;
-      player.update();
-      player.updateCount(playerCount);
-      this.greeting.html("Hello " + player.name)
+
+      this.greeting.html("Hello " + player.name);
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
-this.reset.mousePressed(()=>{
-  player.updateCount(0)
-  game.update(0)
-})
+
+    this.reset.mousePressed(()=>{
+      // Reset locally
+      playerCount = 0;
+      gameState = 0;
+      window.location.reload();
+    });
   }
 }
